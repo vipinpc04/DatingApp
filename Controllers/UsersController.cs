c72502d263b7d5a8b7f6bc474ba556ec90b5e560
Vipin <vipinsekhar04@hotmail.com>
2024-02-28 16:59:05 -0600

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using DatingApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    {
+        var users = await _context.Users.FindAsync(id);
+        return users;
+    }
+
+}
---
+++ Entity/AppUser.cs
@@ -0,0 +1,6 @@
+namespace DatingApp;
+public class AppUser
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
